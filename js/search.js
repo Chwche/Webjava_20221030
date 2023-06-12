4131fb02b508b556bfbe6f8f409b3a7bf4cebfde
최현지 <chj071469@gmail.com>
2023-06-12 17:43:42 +0000

    진짜 7주차

--- js/search.js
+++ js/search.js
@@ -1,13 +1,18 @@
 document.getElementById("search_btn").addEventListener('click',search_message);
 
+var search_array = []; // 빈 배열 – 전역 변수
+
 function search_message(){
-   alert("검색을 수행합니다!"); 
+   	alert("검색을 수행합니다!");
+
 	
 let search_str = document.querySelector("#search_txt"); // 변수에 저장
-    if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
-       alert("검색어가 비었습니다. 입력해주세요"); 
-    }
-    else{
+    if(search_str.value.length === 0)
+		{ // 문자 길이, 엄격한 비교
+		alert("검색어가 비었습니다. 입력해주세요"); 
+    	}
+		else
+		{
 		const filteredText = no_str(search_str.value); // 비속어 필터링
 
     	if (filteredText !== search_str.value)
@@ -15,12 +20,17 @@ let search_str = document.querySelector("#search_txt"); // 변수에 저장
        	alert(`${filteredText}`);
 		}
 		else
+		// 추가구현: 표시되는 배열 10개 제한
 		{
-       	alert("검색을 수행합니다!");
-       	let text = document.getElementById("search_message").innerHTML = search_str.value;
+		if (search_array.length >= 10)
+		{
+		search_array.shift(); //배열의 첫번쨰 요소 삭제
+		}
+		search_array.push(search_str.value); // 배열에 검색어 추가
+       	let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
        	document.querySelector("#form_main").submit();
 		}
-    }
+   		}
 	
 	// 추가구현: 비속어 제거
 	
